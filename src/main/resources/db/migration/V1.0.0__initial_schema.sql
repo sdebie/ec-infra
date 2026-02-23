4182027693cb8c3471bb44c436563acea869f21b
Shawn de Bie <shawn.debie@gamil.com>
2026-02-23 14:29:35 +0200

    Take out seq creating

--- src/main/resources/db/migration/V1.0.0__initial_schema.sql
+++ src/main/resources/db/migration/V1.0.0__initial_schema.sql
@@ -153,9 +153,9 @@ CREATE TABLE shipping_zones (
 );
 
 
-CREATE SEQUENCE IF NOT EXISTS orders_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
-
--- Align the sequence to be ahead of the current max(id) in orders
--- If there are no rows, this will set it to 1 and the first nextval will return 1
-SELECT setval('orders_seq', COALESCE((SELECT MAX(id) FROM orders), 0) + 1, false);
-
+-- CREATE SEQUENCE IF NOT EXISTS orders_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+--
+-- -- Align the sequence to be ahead of the current max(id) in orders
+-- -- If there are no rows, this will set it to 1 and the first nextval will return 1
+-- SELECT setval('orders_seq', COALESCE((SELECT MAX(id) FROM orders), 0) + 1, false);
+--
